cdfa69c980840dfe8c1794b106cced8070013fa5
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-28 16:17:24 -0300

    Base de dados form-contato.sql

--- index.php
+++ index.php
@@ -16,6 +16,10 @@
         <?php
             $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
             var_dump($dados);
+
+            if(!empty($dados['AddMsgCont'])){
+                var_dump($dados);
+            }
         
         ?>
 
