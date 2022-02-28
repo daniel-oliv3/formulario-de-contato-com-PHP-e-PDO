6bc87796c63c6dbd466f29665180d50ddbb34c90
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-28 15:21:45 -0300

    Style.css and Favicon.ico

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de contato com PHP e PDO</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/php.ico"/>
 </head>
 <body>
 
