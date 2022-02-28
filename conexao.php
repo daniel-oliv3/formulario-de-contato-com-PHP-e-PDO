d7b7891660c620c494a9792f4a5d6b50ed781933
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-28 16:40:36 -0300

    Conexão com o banco de dados realizada

--- conexao.php
+++ conexao.php
@@ -7,4 +7,12 @@ $pass = "";
 $dbname = "form-contato_base-dados";
 $port = "3306";
 
+try {
+   $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+   echo "Conexão com o banco de dados realizada com sucesso!";
+} catch(PDOException $err){
+    echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerado "
+    . $err->getMessage();
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include_once './conexao.php';
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
